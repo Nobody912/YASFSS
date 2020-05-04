7a0854f10875a70af80f19f030f9a8c8558fd7e8
Nobody912 <41035180+Nobody912@users.noreply.github.com>
2020-05-04 16:25:17 -0700

    cleaned git ignore, started EncryptedData

--- EncryptedData.java
+++ EncryptedData.java
@@ -7,5 +7,18 @@ import java.util.Base64;
 //Test Comment -Zeke
 
 public class EncryptedData {
-    
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public EncryptedData() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(1024);
+        KeyPair pair = keyGen.generateKeyPair();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+    }
+
+    public void encrypt(String data) {
+        
+    }
 }
