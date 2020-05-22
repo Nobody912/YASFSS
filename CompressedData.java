c3b2971e8a982903f85319965b6c12a1bfb00299
Nobody912 <41035180+Nobody912@users.noreply.github.com>
2020-05-21 21:42:25 -0700

    added compressions stuff

--- CompressedData.java
+++ CompressedData.java
@@ -1,45 +1,57 @@
+import java.io.*;
+import java.util.zip.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
-/**
- * This is all based on a site I saw on compression in Java
- * https://www.codejava.net/java-se/file-io/how-to-compress-files-in-zip-format-in-java
- * 
- * This compresses files into a zip file,which might not be what we want. So
- * I'll try to find something else if we need it.
- * -Zeke
- */
 public class CompressedData
 {
-    public void compress(String filepath)
-    {
-        try 
-        {
-            File file = new File(filepath);
-            String fileName = file.getName().concat(".zip");
-            
-            FileOutputStream fos = new FileOutputStream(fileName);
-            ZipOutputStream zos = new ZipOutputStream(fos);
- 
-            zos.putNextEntry(new ZipEntry(file.getName()));
- 
-            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
-            zos.write(bytes, 0, bytes.length);
-            zos.closeEntry();
-            zos.close();
- 
-        } 
-        catch (FileNotFoundException ex) 
-        {
-            System.err.format("The file %s does not exist", filePath);
-        } 
-        catch (IOException ex) 
-        {
-            System.err.println("I/O error: " + ex);
+    
+    private static void decompressGzipFile(String gzipFile, String newFile) {
+        try {
+            FileInputStream fis = new FileInputStream(gzipFile);
+            GZIPInputStream gis = new GZIPInputStream(fis);
+            FileOutputStream fos = new FileOutputStream(newFile);
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = gis.read(buffer)) != -1) {
+                fos.write(buffer, 0, len);
+            }
+            //close resources
+            fos.close();
+            gis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        
+    }
+
+    private static void compressGzipFile(String file, String gzipFile) {
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            FileOutputStream fos = new FileOutputStream(gzipFile);
+            GZIPOutputStream gzipOS = new GZIPOutputStream(fos){{def.setLevel(Deflater.BEST_COMPRESSION);}};
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len=fis.read(buffer)) != -1) {
+                gzipOS.write(buffer, 0, len);
+            }
+            //close resources
+            gzipOS.close();
+            fos.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
  
     public static void main(String[] args) 
     {
-        String filePath = args[0];
-        zipFile(filePath);
+        String FILENAME = "Abed's Uncontrollable Christmas.mp4";
+        String file = FILENAME;
+        String gzipFile = FILENAME + ".gz";
+        String newFile = "new_" + FILENAME;
+        compressGzipFile(file, gzipFile);
+        decompressGzipFile(gzipFile, newFile);
     }
 }
